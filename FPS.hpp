40a48de842dd99e054baeef91c894b66641384a6
linuxlight <lnxlht4j@gmail.com>
2021-05-09 06:28:26 +0900

    Integrated with GraphicEngine, set time interval available

--- FPS.hpp
+++ FPS.hpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <Windows.h>
 #include "CoordinateSystem.h"
+#include "GraphicEngine.h"
 
 const int KEY_UP = 72;
 const int KEY_DOWN = 80;
@@ -15,12 +16,19 @@ const int KEY_RIGHT = 77;
 
 
 class FramePerSecond {
-private:
 
-    void gotoxy(Point _point);
-    void hideConsoleCursor();
+private:
+    double mvFPS;
+    Console mvConsole;
+    void vGotoXY(Point _point);
+    void vHideConsoleCursor();
+    DWORD vGetUnitTime();
 
 public:
     FramePerSecond();
-    void moveTargetObject(Point target_obj);
+    Console getConsole();
+    void setFPS(double frames);
+    void BuildScreen(int width, int height, int fontw, int fonth);
+    void UpdateScreen();
+    void MoveTargetObject(Point target_obj);
 };
