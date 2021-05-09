29845b7caea231ebbcdf7d2c9f51d70426f31a8e
ilovethisid <ilovethisid@naver.com>
2021-05-09 21:17:10 +0900

    Commit as solution... Might go wrong... In my branch

---
+++ Physics Engine/Object.cpp
@@ -0,0 +1,50 @@
+#include "Object.h"
+
+Object::Object()
+{
+
+}
+
+Object::Object(int _x, int _y)
+{
+	x = _x;
+	y = _y;
+}
+
+int Object::getX()
+{
+	return x;
+}
+
+int Object::getY()
+{
+	return y;
+}
+
+void Object::makeRigidbody()
+{
+	rigidbody = Rigidbody();
+}
+
+void Object::move(vector<Object> objects)
+{
+	rigidbody.collider.move(rigidbody.velocity.getX(), rigidbody.velocity.getY());
+
+	bool collision = false;
+
+	for (int i = 0; i < objects.size(); i++) {
+		if (rigidbody.checkCollision(objects[i]) == 2) {
+			// by moving collider, complete collision occurs
+			collision = true;
+		}
+	}
+
+	if (!collision) {
+		x = x + rigidbody.velocity.getX();
+		y = y + rigidbody.velocity.getY();
+	}
+	else {
+		rigidbody.collider.move(-rigidbody.velocity.getX(), -rigidbody.velocity.getY());
+		// since object didn't move, restore collider
+	}
+}
---
+++ Physics Engine/Object.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+#include "Rigidbody.h"
+
+using namespace std;
+
+class Object
+{
+private:
+	int x;
+	int y;
+
+public:
+	Rigidbody rigidbody;
+
+	Object();
+	Object(int _x, int _y);
+
+	int getX();
+	int getY();
+
+	void makeRigidbody();
+	void move(vector<Object> objects);
+	// move according to velocity
+};
+
