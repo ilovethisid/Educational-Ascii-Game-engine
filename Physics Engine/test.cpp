8f2ad0b1ccdfa4bea8562b3363baf232165a4e10
HunSeok <gnstjr2000@gmail.com>
2021-05-18 15:40:09 +0900

    rollback

---
+++ Physics Engine/test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+#include "GraphicEngine.h"
+
+int main() {
+
+	Console demo;
+	demo.make_console(160, 100, 8, 8);
+	demo.draw_triangle(1, 1, 10, 10, 3, 40);
+
+	int x = 0;
+	int y = 0;
+	int x1 = 100;
+	int y1 = 100;
+	while (1) {
+		demo.set_tmpbufScreen();
+		//MATRIX* pnt_circle = demo.make_circle(10);
+		//demo.tmp_draw_matrix(x++, y++, *pnt_circle);
+		//MATRIX* pnt_square = demo.make_square(6,10);
+		//demo.tmp_draw_matrix(x1--, y1--, *pnt_square);
+		demo.update();
+		Sleep(100);
+
+	}
+}
