3b5448fd894898884deef1ebe2e8705331a4cad2
horizon1030 <51363000+horizon1030@users.noreply.github.com>
2021-05-27 00:51:17 +0900

    Add files via upload

---
+++ objectDB_test main.cpp
@@ -0,0 +1,16 @@
+#include "objectDB.cpp"
+
+int main()
+{
+	Object test_o[2];//저장할 onject의 리스트
+	Object a(1, 2);
+	Matrix _image(10, 11);
+	a.makeImage(_image);
+	Object b(5, 6);
+	Matrix _image1(97, 40);
+	b.makeImage(_image1);
+	test_o[0] = a; test_o[1] = b;
+	objectsave(test_o, 2);
+	Object test_r[2];//저징될 리스트
+	objectread(test_r, 2);
+}
